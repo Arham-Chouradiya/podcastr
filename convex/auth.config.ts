4b7504563df83f7f339183bdfa3b65c3bbbd5985
Arham Chouradiya <chouradi@usc.edu>
2024-06-24 16:49:27 -0700

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://subtle-condor-18.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://subtle-condor-18.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
